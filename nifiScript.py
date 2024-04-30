73b675a9b47a6f19e357c172eff32b6be4fff522
Carlos Dye II <62572729+Incrision@users.noreply.github.com>
2024-04-29 21:03:29 -0700

    Update nifiScript.py

--- nifiScript.py
+++ nifiScript.py
@@ -14,14 +14,22 @@
 from org.apache.commons.io import IOUtils
 from java.nio.charset import StandardCharsets
 from org.apache.nifi.processor.io import StreamCallback
+import time 
     
 # Define a subclass of StreamCallback for use in session.write()
 class PyStreamCallback(StreamCallback):
     def __init__(self):
         pass
     def process(self, inputStream, outputStream):
-        text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-        outputStream.write(bytearray(text.encode('utf-8')))
+		ts = time.time()
+		text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+		text1, text2 = text.split('\n', 1)
+		
+		text1 = "time, " + text1 + "\n"
+		text2 = str(ts) + ", " + text2
+		
+		text = text1 + text2
+		outputStream.write(bytearray(text.encode('utf-8')))
 # end class
 flowFile = session.get()
 if(flowFile != None):
